f35c847ebd22751f69ee0ac2275a46e7ad84ed86
Guy Coleman <coleman.guyg@gmail.com>
2021-10-18 23:36:03 +0200

    .

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -6,6 +6,22 @@ public class Delivery : MonoBehaviour
 {
     bool packageCollected; 
 
+    [SerializeField]
+    float destroyDelay = 0.3f;
+
+    [SerializeField]
+    Color32 hasPackageColour = new Color32(1, 1, 1, 1);
+
+    [SerializeField]
+    Color32 noPackageColour = new Color32(1, 255, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!!");
@@ -17,12 +33,16 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package Collected");
             packageCollected = true;    
+            spriteRenderer.color = hasPackageColour;
+
+            Destroy(other.gameObject, destroyDelay);          
         }
 
         if(other.tag == "Customer" && packageCollected)
         {
             Debug.Log("Package Delivered"); 
             packageCollected = false;
+            spriteRenderer.color = noPackageColour;
         }
 
     }
